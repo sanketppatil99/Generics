6ceee2c029649bd8ec5403028245df7617778f49
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-12 13:35:54 +0530

    [Sanket_Patil] : Print Array Element Using Generics Class

---
+++ Generics/Generics/PrintArray.cs
@@ -0,0 +1,21 @@
+﻿namespace Generics
+{
+    public class PrintArray<T>
+    {
+        private T[] inputArray;
+
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+
+        public void toPrint()
+        {
+            foreach(var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(".........................");
+        }
+    }
+}
--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -4,23 +4,16 @@ namespace Generics
 {
     class Program
     {
-        public static void toPrint<T>(T[] inputArray)
-        {
-            foreach (var element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine(".............................");
-        }
+       
         public static void Main(string[] args)
         {
             int[] intArray = { 1, 2, 3, 4, 5 };
             double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 ,5.5};
             char[] charArray = { 'H','E','L','L','O' };
 
-            Program.toPrint<int>(intArray);
-            Program.toPrint<double>(doubleArray);
-            Program.toPrint<char>(charArray);
+            new PrintArray<int>(intArray).toPrint();
+            new PrintArray<double>(doubleArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();
         }
 
         
