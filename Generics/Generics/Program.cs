8c2caad9c12b174769b8afc838ab55c79c20c13c
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-12 11:11:59 +0530

    [Sanket_Patil] : Print Array Element Without Using Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Generics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H','E','L','L','O' };
+
+            Program.toPrint(doubleArray);
+            Program.toPrint(charArray);
+        }
+
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("...............................");
+        }
+        
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (double element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("...............................");
+        }
+    }
+}
