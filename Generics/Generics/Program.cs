8feba30b352f301a17465d4cec9950cd682e4ab5
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-12 13:13:59 +0530

    [Sanket_Patil] : Print Array Element Using Generics

--- Generics/Generics/Program.cs
+++ Generics/Generics/Program.cs
@@ -4,31 +4,27 @@ namespace Generics
 {
     class Program
     {
+        public static void toPrint<T>(T[] inputArray)
+        {
+            foreach (var element in inputArray)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(".............................");
+        }
         public static void Main(string[] args)
         {
-            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 ,5.5};
             char[] charArray = { 'H','E','L','L','O' };
 
-            Program.toPrint(doubleArray);
-            Program.toPrint(charArray);
+            Program.toPrint<int>(intArray);
+            Program.toPrint<double>(doubleArray);
+            Program.toPrint<char>(charArray);
         }
 
-        public static void toPrint(double[] inputArray)
-        {
-            foreach (double element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("...............................");
-        }
         
-        public static void toPrint(char[] inputArray)
-        {
-            foreach (double element in inputArray)
-            {
-                Console.WriteLine(element);
-            }
-            Console.WriteLine("...............................");
-        }
+
+       
     }
 }
