350977cd8ca9758b447d570e07c19726a6977cdb
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-14 06:13:32 +0530

    [Sanket_Patil] : Given Max Number at 1st Position return the Same Number

---
+++ FindMaximum/FindMaximum/Program.cs
@@ -0,0 +1,17 @@
+﻿using FindMaximumProblem;
+
+namespace FindMaximum
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            int c = TestCaseOne.MaxNumber(59, 45, 56);
+
+
+            Console.WriteLine("Maximum Number is : {0}", c);
+
+        }
+    }
+}
