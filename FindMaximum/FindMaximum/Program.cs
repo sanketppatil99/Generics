b879c52a074a1faaf0abcfd041cb5d247d98d088
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-14 06:47:55 +0530

    [Sanket_Patil] : Given Max float Number at 2nd Position return the Same Number

--- FindMaximum/FindMaximum/Program.cs
+++ FindMaximum/FindMaximum/Program.cs
@@ -6,8 +6,8 @@ namespace FindMaximum
     {
         public static void Main(string[] args)
         {
-            //Test case 1.1
-            float c = TestCaseOne.MaxNumber(57.25f, 39.65f, 50.58f);
+            //Test case 1.2
+            float c = TestCaseOne.MaxNumber(39.25f, 57.25f, 50.58f);
 
 
             Console.WriteLine("Maximum Number is : {0}", c);
