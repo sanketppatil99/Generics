dfff61ad2626cbe2c5ffd03b377c136dbaf8bdbf
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-17 00:14:30 +0530

    [Sanket_Patil] : Refactor all 3 to One Generic Method

--- FindMaximum/FindMaximum/Program.cs
+++ FindMaximum/FindMaximum/Program.cs
@@ -7,12 +7,21 @@ namespace FindMaximum
     {
         public static void Main(string[] args)
         {
-            //Test case 1.3
-            string c = TestCaseOne.MaxNumber("Apple", "Banana", "Peach"); 
+
+            Console.WriteLine("Welcome to Find Maximum Problem using Generics");
+
+            double n = RefactorCode1<double>.MaxValue(50.5, 45.6, 48.5);
+            int a = RefactorCode1<int>.MaxValue(70, 60, 50);
+            string c = RefactorCode1<string>.MaxValue("Peach", "Apple", "Banana");
 
 
-            Console.WriteLine("Maximum Number is : {0}", c);
 
+            Console.WriteLine("Maximum int in is: {0}", a);
+            Console.WriteLine("Maximum Number in float is: {0}", n);
+            Console.WriteLine("Maximum value in string is: {0}", c);
         }
+
+
     }
 }
+
---
+++ FindMaximum/FindMaximum/RefactorCode1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class RefactorCode1<T> where T : IComparable
+    {
+        public static T MaxValue(T firstNumber, T secondNumber, T thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new Exception("All value are same");
+
+        }
+    }
+}
